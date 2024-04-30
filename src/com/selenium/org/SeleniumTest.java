dd0e72cc3cca4eb63116ca875d3e82a9dd2bf099
krishnaveniram@gmail.com <vkrishnaveniram.com>
2024-04-30 22:09:38 +0530

    this is first commit

---
+++ src/com/selenium/org/SeleniumTest.java
@@ -0,0 +1,13 @@
+package com.selenium.org;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\medha\\chromeDriver\\chrome32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		WebDriver driver1 = new FirefoxDriver();
+
+	}
+
+}
